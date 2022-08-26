bf5cb3cde19828fd7c9cd5e97db523874ed580b0
Sticky_joe <jmugo16@gmail.com>
2022-08-26 16:26:40 +0300

    This comitt will login the user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
-import user from "../models/user.js";
+import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
+//REGISTER
 export const register = async (req, res, next) =>{
 try {
 
@@ -21,3 +22,24 @@ try {
 }
 
 }
+
+//LOGIN
+export const login = async (req, res, next) =>{
+  try {
+  
+   const user = await User.findOne({ username: req.body.username});
+   if(!user) return next(createError(404, "User not found!"));
+
+   const isPasswordCorrect = await bcrypt.compare(
+    req.body.password,
+    user.password
+   );
+   if(!isPasswordCorrect) return next(createError(404, "Wrong username or password!"));
+
+   res.status(200).json(user);
+ 
+  } catch (err) {
+    next(err)
+  }
+  
+  };
