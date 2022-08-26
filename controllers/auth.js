91c1e587b4259f277bedef044398b434462013b0
Sticky_joe <jmugo16@gmail.com>
2022-08-26 15:20:35 +0300

    This commit will Create a new user

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+import user from "../models/user.js";
+export const register = async (req, res, next) =>{
+try {
+  const newUser = new user({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  await newUser.save()
+  res.status(200).send("User has been created Successfully");
+  
+} catch (err) {
+  next(err)
+}
+
+}
