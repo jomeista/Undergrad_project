5fa1dbc1e960f98bdc0e3ec3df4335cdd9a70216
Sticky_joe <jmugo16@gmail.com>
2022-09-12 08:47:27 +0300

    This commit will add Users routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
 
 const router = express.Router ();
 
+//ROUTES
 
+router.put("/:id", updateUser) //UPDATE
+
+router.delete("/:id", deleteUser) //DELETE
+
+router.get("/:id", getUser) //GET
+
+router.get("/", getUsers) //GETALL
 
 export default router
 
