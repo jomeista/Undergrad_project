814af4f8562bdacffcd8e972fc57b72800ed4fa2
Sticky_joe <jmugo16@gmail.com>
2022-10-17 22:40:09 +0300

    This commit will add CSS styles to the Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,8 +2,17 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">PeteLiz Hostel</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
-export default Navbar
+export default Navbar;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 
+
 const Home = () =>{
   return (
-    <div><Navbar /></div>
-  )
-}
+    <div>
+    <Navbar />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
