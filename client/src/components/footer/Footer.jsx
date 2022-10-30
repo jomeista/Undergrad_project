7890a933669433ef5ac3abe1cd58463a307bf2e9
Sticky_joe <jmugo16@gmail.com>
2022-10-30 17:38:01 +0300

    This commit will add a footer component

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import './footer.css';
+
+const Footer = () =>{
+  return (
+    <div className='footer'>
+    <div className='fLists'>
+      <ul className='fList'>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+      </ul>
+      <ul className='fList'>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+      </ul>
+      <ul className='fList'>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+      </ul>
+      <ul className='fList'>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+      </ul>
+      <ul className='fList'>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+        <li className='fListItem'>Countries</li>
+      </ul>
+    </div>
+<div className='fText'>Copyright © 2022 PeteLiz</div>
+    </div>
+  )
+}
+
+export default Footer
