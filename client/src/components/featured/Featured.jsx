c8f464c333525bf58a484a2fe5d162307934f888
Sticky_joe <jmugo16@gmail.com>
2022-11-07 19:08:27 +0300

    This commit will add hooks to Featured component

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,31 +1,40 @@
+import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () =>{
+
+  const {data, loading, error,} = useFetch("/hostel/countByCity?cities=Kisumu,Nairobi");
+
+  
   return (
 <div className="featured">
-  <div className="featuredItem">
+  {loading ? (
+    "Loading please wait") : (
+    <>
+    
+    <div className="featuredItem">
     <img src="https://i.pinimg.com/originals/73/1e/94/731e945958ba0e0d96881937f9671d19.jpg" alt="" className="featuredImg"/>
     <div className="featuredTitles">
-      <h1>Nairobi West</h1>
-      <h2>123 properties</h2>
+      <h1>Kisumu</h1>
+      <h2>{data[0]} properties</h2>
     </div>
   </div>
 
   <div className="featuredItem">
     <img src="https://i.pinimg.com/originals/73/1e/94/731e945958ba0e0d96881937f9671d19.jpg" alt="" className="featuredImg"/>
     <div className="featuredTitles">
-      <h1>Nairobi West</h1>
-      <h2>123 properties</h2>
+      <h1>Kisumu</h1>
+      <h2>{data[1]} properties</h2>
     </div>
   </div>
 
   <div className="featuredItem">
     <img src="https://i.pinimg.com/originals/73/1e/94/731e945958ba0e0d96881937f9671d19.jpg" alt="" className="featuredImg"/>
     <div className="featuredTitles">
-      <h1>Nairobi West</h1>
-      <h2>123 properties</h2>
+      <h1>Nairobi</h1>
+      <h2>{data[1]} properties</h2>
     </div>
-  </div>
+  </div></>)}
 </div>
   );
 };
