4d7bf3c7822130a57b9b05e0ef92d62640340782
Sticky_joe <jmugo16@gmail.com>
2022-11-10 23:05:20 +0300

    This commit will resolve the dates on List component

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -15,7 +15,7 @@ const List = () =>{
   const location = useLocation();
 
   const [destination, setDestination] = useState(location.state.destination);
-  const [date, setDate] = useState(location.state.date);
+  const [dates, setDates] = useState(location.state.dates);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
   const [min, setMin] = useState(undefined);
@@ -44,13 +44,13 @@ const List = () =>{
           <div className="lsItem">
             <label>Check in date</label>
             <span onClick ={() => setOpenDate(!openDate)}>
-            {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}
+            {`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(dates[0].endDate, "MM/dd/yyyy")}`}
             </span>
             { openDate && (
               <DateRange 
-              onChange = {item => setDate([item.selection])}
+              onChange = {item => setDates([item.selection])}
               minDate ={new Date()}
-              ranges ={date}
+              ranges ={dates}
             />)}
           </div>
           <div className="lsItem">
