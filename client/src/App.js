d1b8f2c6b4fb9a2c10c1fa478dfd8e8c4b21637e
Sticky_joe <jmugo16@gmail.com>
2022-11-12 00:03:08 +0300

    This commit will add a login in component

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,17 @@ import {
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Hostel from "./pages/hostel/Hostel";
+import Login from "./pages/login/Login";
 function App() {
   return (
      
     //Routes
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/hostel" element={<List />}></Route>
-        <Route path="/hostel/:id" element={<Hostel />}></Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="/hostel" element={<List />}/>
+        <Route path="/hostel/:id" element={<Hostel />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </BrowserRouter>
   )
